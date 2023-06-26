fd51c9170e679a386e228cf12de16239ba19963e
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-26 13:00:50 +0200

    macro-components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script>
-import  from './components/.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/.vue';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+        }
+    },
+
+    components: {
+    AppHeader,
+    AppMain,
+    Footer
+  }
+}
+
 </script>
 
 <template>
   
-  </>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>
+            Header
+        </h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss">
+    
+</style>
