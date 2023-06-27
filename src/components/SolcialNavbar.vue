4b45d58ecb34b0b97218faca5b0be9ad45ee6e02
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-27 08:35:17 +0200

    added icon hover lightgrey

--- src/components/SolcialNavbar.vue
+++ src/components/SolcialNavbar.vue
@@ -4,7 +4,7 @@
             <div class="col-12 d-flex direction-row justify-content-between my-3">
                 <div class="d-flex direction-row">
                     <div class="pe-2">
-                        <i class="fa-solid fa-phone-flip" style="color: #ffffff;"></i>
+                        <i class="fa-solid fa-phone-flip"></i>
                     </div>
                     <p>
                         Call us for a Free Quote: 1.800.555.6789
@@ -14,17 +14,20 @@
                 <!--* FARLO DIVENTARE UN ARRAY-->
                 <!--? fare cmponet icons-->
                 <div class="d-flex direction-row">
-                    <div class="pe-2">
-                        <i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i>
+                    <div class="pe-3">
+                        <i class="fa-brands fa-facebook-f" ></i>
                     </div>
-                    <div>
-                        <i class="fa-brands fa-twitter" style="color: #ffffff;"></i>
+
+                    <div class="pe-3">
+                        <i class="fa-brands fa-twitter"></i>
                     </div>
-                    <div>
-                        <i class="fa-brands fa-instagram" style="color: #ffffff;"></i>
+
+                    <div class="pe-3">
+                        <i class="fa-brands fa-instagram" ></i>
                     </div>
-                    <div>
-                        <i class="fa-brands fa-youtube" style="color: #ffffff;"></i>
+
+                    <div class="pe-3">
+                        <i class="fa-brands fa-youtube" ></i>
                     </div>
                 </div>
             </div>
@@ -53,5 +56,9 @@ export default {
         background-color: #6abf17;
         color: white;
         height: 60px;
+    }
+
+    i:hover{
+        color: lightgrey;
     }
 </style>
