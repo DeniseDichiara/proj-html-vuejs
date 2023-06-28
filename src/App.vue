db14845d0f0b69b6ed5e643263524cded5ecd3ca
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-28 17:27:39 +0200

    added fixed element

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@ import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
-    name: 'App',
-    data() {
-        return {
-        }
-    },
+  name: 'App',
+  data() {
+    return {
+    }
+  },
 
-    components: {
+  components: {
     AppHeader,
     AppMain,
     AppFooter
@@ -19,15 +19,36 @@ export default {
 </script>
 
 <template>
-  
-  <AppHeader/>
-  <AppMain/>
-  <AppFooter/>
+  <div class="fixed">
+    <div class="pt-4 py-2 ">
+      <div class="d-flex flex-row justify-content-center">
+        <i class="fa-solid fa-cart-shopping"></i>
+      </div>
+      <div class="d-flex flex-row justify-content-center">
+        <p>
+          Buy Now
+        </p>
+      </div>
+      
+
+    </div>
+  </div>
+
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 <style lang="scss">
-
 @import "bootstrap/scss/bootstrap";
 @import './styles/general.scss';
 
+.fixed {
+  position: fixed;
+  top: 250px;
+  right: 20px;
+  width: 100px;
+  background-color: white;
+  border-radius: 10px;
+}
 </style>
