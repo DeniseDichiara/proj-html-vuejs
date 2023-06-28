cbe8f1fb58defcb27215924b90e2c321bc364d7b
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-28 15:54:12 +0200

    update height px

--- src/components/AppQuote.vue
+++ src/components/AppQuote.vue
@@ -4,7 +4,7 @@
         <div>
             <QuoteTitle />
         </div>
-        <div class=" d-flex justify-content-center py-5">
+        <div class=" d-flex justify-content-center py-5 ">
             <button type="button" class=" my_btn-color my_btn">
                 CLICK HERE
             </button>
@@ -28,7 +28,7 @@ export default {
 
 <style lang="scss">
 .quote-bg {
-    height: 400px;
+    height: 500px;
     width: 1640px;
     background-image: url('/img/avada-movers-request-quote-background.jpg');
     background-size: cover;
