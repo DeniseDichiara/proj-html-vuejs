b2de89cff934c0a9d1b9c6ba3db9f71524939853
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-28 15:22:44 +0200

    upgrade AppHeadache free

--- src/components/AppHeadache.vue
+++ src/components/AppHeadache.vue
@@ -5,25 +5,32 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="mx-auto">
-                            <p class="p-grey text-uppercase my_moving">
-                                headeache free
-                            </p>
-
-                            <h2 class=" my_moving fw-bold">
-                                Award Wining
-                            </h2>
-                            <h2 class="my_moving fw-bold margin-h2">
-                                MovingCompany
-                            </h2>
-
-                            <p class="p-grey text-uppercase my_moving">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque ipsum nihil dignissimos
-                                maiores praesentium. Tempore distinctio inventore necessitatibus molestiae, earum explicabo
-                                quibusdam nulla eveniet iure laboriosam eum possimus a. Sit.
-                            </p>
                             <div>
-                                <button type="button" class="my_btn-color my_btn">READ OUR TESTIMONIALS</button>
+                                <p class="p-grey text-uppercase my_moving">
+                                    headeache free
+                                </p>
+
+                                <h2 class=" my_moving fw-bold">
+                                    Award Wining
+                                </h2>
+                                <h2 class="my_moving fw-bold margin-h2">
+                                    MovingCompany
+                                </h2>
                             </div>
+
+                            <div class="my_wining mx-auto">
+                                <div class="d-flex justify-content-center">
+                                    <p class="p-grey text-uppercase my_moving text-center">
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque ipsum nihil
+                                        dignissimos
+                                        maiores praesentium.
+                                    </p>
+                                </div>
+                                <div class="d-flex justify-content-center mt-3">
+                                    <button type="button" class="my_btn-color my_btn">READ OUR TESTIMONIALS</button>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -43,5 +50,9 @@ export default {
 <style lang="scss">
 .headache {
     background-color: #f8f9fd;
+}
+
+.my_wining {
+    width: 600px;
 }
 </style>
